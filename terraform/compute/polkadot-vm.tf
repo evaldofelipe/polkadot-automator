f0f86bfa57c308a484a0a444353c057eddad54bd
Evaldo <contato@evaldofelipe.com>
2020-02-09 15:41:41 -0300

    Create isolated network for polkadot
    Deploy a new vnet, subnet and firewall rule for the environment

--- terraform/compute/polkadot-vm.tf
+++ terraform/compute/polkadot-vm.tf
@@ -8,6 +8,20 @@ terraform {
   required_version = "0.11.13"
 }
 
+resource "google_compute_network" "vpc_network" {
+  name                    = "vpc-polkadot-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "polkadot_subnet" {
+  name          = "polkadot-subnetwork"
+  ip_cidr_range = "10.100.10.0/27"
+  region        = "${var.subnet_location}"
+  network       = "${google_compute_network.vpc_network.self_link}"
+
+  depends_on = ["google_compute_network.vpc_network"]
+}
+
 resource "google_compute_instance" "polkadot_vm" {
   name         = "polkadot-vm"
   machine_type = "n1-standard-1"
@@ -20,10 +34,42 @@ resource "google_compute_instance" "polkadot_vm" {
   }
 
   network_interface {
-    network = "default"
+    network    = "vpc-polkadot-network"
+    subnetwork = "polkadot-subnetwork"
 
     access_config {
       // Ephemeral IP
     }
   }
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "groupadd -r polkadot",
+  #     "useradd -m -r -g polkadot polkadot",
+  #     "usermod -aG sudo polkadot",
+  #   ]
+  # }
+
+  metadata = {
+    ssh-keys = "polkadot:${file(var.pub_key_path)}"
+  }
+  depends_on = ["google_compute_subnetwork.polkadot_subnet"]
+}
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name    = "allow-ssh-from-specified-cidr-range"
+  project = "${var.project_name}"
+  network = "vpc-polkadot-network"
+
+  allow {
+    protocol = "tcp"
+
+    ports = [
+      "22",
+    ]
+  }
+
+  source_ranges = [
+    "${var.cidr_allowed}",
+  ]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,22 @@ variable "location" {
   default     = "us-east1-b"
 }
 
+variable "subnet_location" {
+  description = "The GCP region where the subnet will be create"
+  default     = "us-east1"
+}
+
 variable "project_name" {
   description = "the project ID found at console"
   default     = "polkadot-267705"
+}
+
+variable "cidr_allowed" {
+  description = "Allowed CIDR for ssh conections"
+  default     = "0.0.0.0/0"
+}
+
+variable "pub_key_path" {
+  description = "your ssh key inside the container"
+  default     = "/home/polkadot/.ssh/id_rsa.pub"
 }
